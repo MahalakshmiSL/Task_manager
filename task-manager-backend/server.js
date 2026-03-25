203a33707fed30a28f1869a2e94c0cb095a38a4f
Mahalakshmi S L <mahalakshmi822004@gmail.com>
2026-03-25 14:13:23 +0530

    Initial commit - Full Stack Task Managekkr

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -18,8 +18,7 @@ const app = express();
 app.use("/api/logs", logRoutes);
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    credentials: true,
+    origin: "*",
   })
 );
 app.use(express.json());
